1a61ce65de3f62290cf1d1cfeba67c1994bbcbdd
나황제 <na03266@livewalk.co.kr>
2024-07-24 11:10:03 +0900

    CRUD

--- src/main/java/me/hwangje/smart_farm/controller/BlogApiController.java
+++ src/main/java/me/hwangje/smart_farm/controller/BlogApiController.java
@@ -4,13 +4,11 @@ import lombok.RequiredArgsConstructor;
 import me.hwangje.smart_farm.domain.Article;
 import me.hwangje.smart_farm.dto.AddArticleRequest;
 import me.hwangje.smart_farm.dto.ArticleResponse;
+import me.hwangje.smart_farm.dto.UpdateArticleRequest;
 import me.hwangje.smart_farm.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,15 +17,15 @@ import java.util.List;
 public class BlogApiController {
     private final BlogService blogService;
 
-    @PostMapping("api/articles")
+    @PostMapping("/api/articles")
     public ResponseEntity<Article> addArticle(@RequestBody AddArticleRequest request) {
         Article savedArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
     }
 
-    @GetMapping("api/articles")
-    public ResponseEntity<List<ArticleResponse>> findAllArticles(){
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
         List<ArticleResponse> articles = blogService.findAll()
                 .stream()
                 .map(ArticleResponse::new)
@@ -36,5 +34,28 @@ public class BlogApiController {
         return ResponseEntity.ok()
                 .body(articles);
 
+    }
+
+    @GetMapping("/api/articles/{id}")
+    public ResponseEntity<ArticleResponse> findArticle(@PathVariable long id) {
+        Article article = blogService.findById(id);
+
+        return ResponseEntity.ok()
+                .body(new ArticleResponse(article));
+    }
+
+    @DeleteMapping("/api/articles/{id}")
+    public ResponseEntity<Void> deleteArticle(@PathVariable long id) {
+        blogService.delete(id);
+
+        return ResponseEntity.ok()
+                .build();
+    }
+
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable long id, @RequestBody UpdateArticleRequest request) {
+        Article updateArticle = blogService.update(id, request);
+
+        return ResponseEntity.ok().body(updateArticle);
     }
 }
---
+++ src/main/java/me/hwangje/smart_farm/dto/UpdateArticleRequest.java
@@ -0,0 +1,13 @@
+package me.hwangje.smart_farm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class UpdateArticleRequest {
+    private String title;
+    private String content;
+}
