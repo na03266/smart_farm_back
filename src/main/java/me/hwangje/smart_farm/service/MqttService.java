e01d6bbd7131798b9521b28a9267564e0b3d280e
나황제 <na03266@livewalk.co.kr>
2024-08-23 15:04:21 +0900

    mqtt 수정

--- src/main/java/me/hwangje/smart_farm/service/MqttService.java
+++ src/main/java/me/hwangje/smart_farm/service/MqttService.java
@@ -24,6 +24,11 @@ public class MqttService {
 
         System.out.println("Received message from topic '" + topic + "': " + payload);
 
+        /**
+         * 수신할때 어떤 자료형으로 수신하는지 확인 후에
+         * 각 제어명령에 따라서 수신할것,
+         * 제어명령 setup, device-status, sensor-data
+         */
         // 토픽에 따른 처리 로직
         if (topic.startsWith("sensor/")) {
             // 센서 데이터 처리
@@ -33,6 +38,7 @@ public class MqttService {
         // ... 기타 토픽 처리 ...
     }
 
+    // 이걸 가져다가 다른데서 참조해서 실행시켜야함. 대신 json 구조화 필수
     public void publishMessage(String topic, String payload) {
         Message<String> message = MessageBuilder
                 .withPayload(payload)
